8f1c4f1a8e4a5a421355e500a903d5d77a5f3073
MorcosS <morkos.said@gmail.com>
2017-03-25 20:16:41 +0200

    Materialize this app

---
+++ CaptureReportProject/app/src/main/java/projects/un/com/capturereportproject/ImportReport.java
@@ -0,0 +1,13 @@
+package projects.un.com.capturereportproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImportReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import_report);
+    }
+}
---
+++ ocr-reader/app/src/main/java/com/google/android/gms/samples/vision/ocrreader/DataBase/OCRDatabase.java
@@ -0,0 +1,7 @@
+package com.google.android.gms.samples.vision.ocrreader.DataBase;
+
+/**
+ * Created by MorcosS on 9/10/16.
+ */
+public class OCRDatabase {
+}
